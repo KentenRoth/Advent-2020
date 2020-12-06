f6fb9e9bc7a33731fc43dfc14ca1746f4721e7c3
Kent Roth <kentencroth@yahoo.com>
2020-12-06 10:26:59 -0600

    Day 1 Star 2 completed

--- day1.js
+++ day1.js
@@ -19,3 +19,24 @@ equals2020 = (num1, num2) => {
 };
 
 findingSum(data);
+
+// what 3 numbers added together equal 2020 then mulitpled by each other
+findingSum2 = (array) => {
+	for (let i = 0; i < array.length; i++) {
+		for (let j = 0; j < array.length; j++) {
+			for (let k = 0; k < array.length; k++) {
+				let sum = array[i] + array[j] + array[k];
+				if (sum === 2020) {
+					multiply3numbers(array[i], array[j], array[k]);
+					return console.log(array[i], array[j], array[k]);
+				}
+			}
+		}
+	}
+};
+
+multiply3numbers = (num1, num2, num3) => {
+	console.log(num1 * num2 * num3);
+};
+
+findingSum2(data);
