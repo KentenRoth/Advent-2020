bd0d73310dd44d1e3c775e136f86b7e7cb165ead
Kent Roth <kentencroth@yahoo.com>
2020-12-07 16:09:55 -0600

    Day 2 Completed

--- day2.js
+++ day2.js
@@ -5,6 +5,9 @@ const util = require('util');
 
 const read = util.promisify(fs.readFile);
 
+let validPassword = 0;
+let passwordValidation = 0;
+
 // Getting Values
 getValues = async () => {
 	const values = await read('./data/day2Data.txt', 'utf8').then((data) =>
@@ -12,11 +15,40 @@ getValues = async () => {
 	);
 	values.forEach((passwords) => {
 		const getValues = passwords.split(' ');
-		const numbers = getValues[0];
-		const passwordKey = getValues[1];
+		const numbers = getValues[0].split('-');
+		const passwordKeys = getValues[1].split(':');
+
+		const smallNumber = parseInt(numbers[0]);
+		const largeNumber = parseInt(numbers[1]);
+		const passwordKey = passwordKeys[0];
 		const password = getValues[2];
-		console.log(numbers, ' ', passwordKey, ' ', password);
+		isValidPassword1(smallNumber, largeNumber, passwordKey, password);
+		isValidPassword2(smallNumber, largeNumber, passwordKey, password);
 	});
+};
+
+isValidPassword1 = (smallNum, largeNum, key, pass) => {
+	let keyInString = 0;
+	for (let i = 0; i < pass.length; i++) {
+		if (pass[i] === key) {
+			keyInString = keyInString + 1;
+		}
+	}
+	if (keyInString >= smallNum && keyInString <= largeNum) {
+		validPassword = validPassword + 1;
+	}
+};
+
+// position 1 (4) or postion 2 (8) must be the key (g) in the password (ggtxgtgbg)
+isValidPassword2 = (smallNum, largeNum, key, pass) => {
+	if (pass[smallNum - 1] === key && pass[largeNum - 1] === key) {
+		return;
+	}
+
+	if (pass[smallNum - 1] === key || pass[largeNum - 1] === key) {
+		passwordValidation = passwordValidation + 1;
+	}
+	console.log(passwordValidation);
 };
 
 getValues();
