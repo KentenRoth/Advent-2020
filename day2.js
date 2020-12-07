29a4fbb1da6948f3a965aee660bbe5c84d36a85c
Kent Roth <kentencroth@yahoo.com>
2020-12-07 13:05:07 -0600

    Getting Values sepearted

--- day2.js
+++ day2.js
@@ -1,2 +1,22 @@
 // 4-8 g: ggtxgtgbg
 // the specified letter (g) must appear in the password (ggtxgtgbg) between the numbers specified (4-8)
+const fs = require('fs');
+const util = require('util');
+
+const read = util.promisify(fs.readFile);
+
+// Getting Values
+getValues = async () => {
+	const values = await read('./data/day2Data.txt', 'utf8').then((data) =>
+		data.split('\n')
+	);
+	values.forEach((passwords) => {
+		const getValues = passwords.split(' ');
+		const numbers = getValues[0];
+		const passwordKey = getValues[1];
+		const password = getValues[2];
+		console.log(numbers, ' ', passwordKey, ' ', password);
+	});
+};
+
+getValues();
