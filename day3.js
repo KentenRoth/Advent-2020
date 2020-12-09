c184f514f72f7dfab31deaab68eb1403d1050762
Kent Roth <kentencroth@yahoo.com>
2020-12-09 17:28:26 -0600

    Star number 1

--- day3.js
+++ day3.js
@@ -1,1 +1,25 @@
+const fs = require('fs');
+const util = require('util');
 
+const read = util.promisify(fs.readFile);
+
+const eachRow = 31;
+let x = 0;
+
+letsGoSledding = async () => {
+	let tree = 0;
+	const treeOrFree = await read('./data/day3Data.txt', 'utf8').then((data) =>
+		data.split('\n')
+	);
+	for (let i = 0; i < treeOrFree.length; i++) {
+		let xLocation = x % eachRow;
+		let isItATree = treeOrFree[i].split('');
+		if (isItATree[xLocation] === '#') {
+			tree++;
+		}
+		x += 3;
+	}
+	console.log(tree);
+};
+
+letsGoSledding();
