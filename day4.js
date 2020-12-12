3e0769e34e1b881a1aecf025434d9ff9bd23df14
Kent Roth <kentencroth@yahoo.com>
2020-12-11 18:53:19 -0600

    Star 2 Completed
    Ugliest code ever writtten.  But it works....  I'll come back and refactor this.

--- day4.js
+++ day4.js
@@ -5,7 +5,6 @@ const getData = fs.readFileSync('./data/day4Data.txt', 'utf-8');
 const passports = getData.split('\n\n').map((x) => x.split('\n'));
 
 getPassportData = (eachPassport) => {
-	var neededKeys = ['byr', 'iyr', 'eyr', 'hgt', 'hcl', 'ecl', 'pid'];
 	let valid = 0;
 	eachPassport.forEach((passports) => {
 		let passport = passports
@@ -21,3 +20,70 @@ getPassportData = (eachPassport) => {
 };
 
 getPassportData(passports);
+
+getPassportData2 = (eachPassport) => {
+	let valid = 0;
+	eachPassport.forEach((passports) => {
+		let count = 0;
+		let passport = passports
+			.join(' ')
+			.split(' ')
+			.sort()
+			.map((key) => key.split(':'))
+			.filter((x) => x[0] !== 'cid');
+		if (passport.length >= 7) {
+			if (passport[0][1] <= 2002 && passport[0][1] >= 1920) {
+				count++;
+			}
+			if (
+				passport[1][1] === 'amb' ||
+				passport[1][1] === 'blu' ||
+				passport[1][1] === 'brn' ||
+				passport[1][1] === 'gry' ||
+				passport[1][1] === 'grn' ||
+				passport[1][1] === 'hzl' ||
+				passport[1][1] === 'oth'
+			) {
+				count++;
+			}
+			if (passport[2][1] >= 2020 && passport[2][1] <= 2030) {
+				count++;
+			}
+
+			if (/^#[0-9,a-f]+/.test(passport[3][1]) === true) {
+				count++;
+			}
+
+			if (
+				passport[4][1].slice(-2) === 'in' &&
+				passport[4][1].length === 4 &&
+				passport[4][1].slice(0, 2) >= 59 &&
+				passport[4][1].slice(0, 2) <= 76
+			) {
+				count++;
+			}
+			if (
+				passport[4][1].slice(-2) === 'cm' &&
+				passport[4][1].length === 5 &&
+				passport[4][1].slice(0, 3) >= 150 &&
+				passport[4][1].slice(0, 3) <= 193
+			) {
+				count++;
+			}
+
+			if (passport[5][1] >= 2010 && passport[5][1] <= 2020) {
+				count++;
+			}
+			if (/^\d{9}$/.test(passport[6][1]) === true) {
+				count++;
+			}
+
+			if (count === 7) {
+				valid++;
+			}
+		}
+	});
+	console.log(valid);
+};
+
+getPassportData2(passports);
